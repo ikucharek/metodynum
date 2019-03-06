7fa4686a2388bf5274ab5c0e0efac9505b786d12
Izabela <48252451+ikucharek@users.noreply.github.com>
2019-03-06 19:19:55 +0100

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -13,7 +13,7 @@ from matplotlib.pyplot import *
 #1 calculate & print the value of function y = 2x^2 + 2x + 2 for x=[56, 57, ... 100] (0.5p)
 
 l=[]
-for i in range(56,100):
+for i in range(56,101):
     y = 2*i**2 + 2*i + 2
     l.append(y)
 print(l)
